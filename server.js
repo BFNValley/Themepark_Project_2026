44d89702d9d3155379c8cb703a41ff96f315b4ce
BFNValley <146040046+BFNValley@users.noreply.github.com>
2026-03-12 21:51:35 -0500

    testing login page

--- server.js
+++ server.js
@@ -4,6 +4,7 @@ const port = process.env.PORT || 4000
 
 const app = express();
 app.use(express.static("docs"));
+app.use(express.json());
 
 const config = {
     user: process.env.DB_USER,
@@ -13,6 +14,7 @@ const config = {
     options: { encrypt: true }
 };
 
+/*
 app.get("/customers", async (req, res) => {
     try{
         await sql.connect(config);
@@ -22,6 +24,27 @@ app.get("/customers", async (req, res) => {
         res.send(err);
     }
 });
+*/
+
+app.get("/", (req,res)=>{
+    res.sendFile(__dirname+"/docs/login.html")
+});
+
+app.post("/login", (req, res) => {
+    const role = req.body.role;
+
+    if(role === "customer"){
+        res.json({redirect: "/customer.html"});
+    }
+
+    else if(role === "employee"){
+        res.json({redirect: "/employee.html"});
+    }
+
+    else{
+        res.status(400).send("Invalid role");
+    }
+})
 
 app.listen(port, () => {
     console.log("Server running on port 4000");
